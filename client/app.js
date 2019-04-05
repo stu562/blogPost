c49f082cc1eb9ce1646d46fb0793da2b4b4a565e
Steven Tu <stu562@gmail.com>
2019-04-05 12:56:02 -0700

    added require methods

---
+++ client/app.js
@@ -0,0 +1,9 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+
+//title 
+//image
+//body
+//crud operation 
