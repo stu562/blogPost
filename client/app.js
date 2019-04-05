ff2b423c68771be797c9fed0e3b8f5c905a8df4b
Steven Tu <stu562@gmail.com>
2019-04-05 12:59:24 -0700

    added middleware

--- client/app.js
+++ client/app.js
@@ -3,6 +3,10 @@ var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 
+mongoose.connect('mongodb://localhost/blogPost');
+app.set("view engiene", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 //title 
 //image
 //body
